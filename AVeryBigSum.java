99dc05a3dbad21409d2e6e17ecf69e823aa51c32
David Kenyon <davidkenyon61@gmail.com>
2018-10-16 10:53:10 -0400

    Added hackerrank description comment

--- AVeryBigSum.java
+++ AVeryBigSum.java
@@ -1,4 +1,4 @@
-//Reference: https://www.hackerrank.com/challenges/a-very-big-sum/problem
+//Reference: https://www.hackerrank.com/challenges/a-very-big-sum/problem 
 //"Calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large."
 import java.io.*;
 import java.math.*;
