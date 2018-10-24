f0ad2effaed03101624cf1f677148c7ca6e20ce9
David Kenyon <davidkenyon61@gmail.com>
2018-10-24 16:13:15 -0400

    Commit Staged

---
+++ PrintIfAndElse.java
@@ -0,0 +1,12 @@
+package misc;
+
+//Program that will print 'false true', visiting both if and else
+public class PrintIfAndElse {
+	public static void main (String [] args)
+	{
+		if (System.out.printf("%s","false") == null)
+			System.out.println("false");			
+		else
+			System.out.println("true");
+	}
+}
