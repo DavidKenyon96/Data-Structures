aa57721c1106f2f7e78e4647c0c3eceeb4ff55bd
David Kenyon <davidkenyon61@gmail.com>
2018-11-01 13:32:11 -0400

    Fixed formatting

--- ManualExponentiaion.java
+++ ManualExponentiaion.java
@@ -1,17 +1,13 @@
 //Program that finds the result of a base number set to a power without using Math.pow
 public class ManualExponentiaion {
-
-	
 	public int exponentiation(int number, int exponent)
 	{
-		
 		int temp = 1;
 		
 		for (int i=0; i<exponent; i++)
 		{
 			temp*=number;
 		}
-		
 		return temp;
 	}
 	
